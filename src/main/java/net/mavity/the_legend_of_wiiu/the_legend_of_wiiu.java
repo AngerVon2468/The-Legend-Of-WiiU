60f0510e0356aff5f2deded13b650f346e4553b5
AngerVon2468 <thomaschad0909@gmail.com>
2024-02-11 10:55:56 +1100

    Added code to put The Master Sword in the TLOW Creative Mode Tab
    Signed-off-by: AngerVon2468 <thomaschad0909@gmail.com>

--- src/main/java/net/mavity/the_legend_of_wiiu/the_legend_of_wiiu.java
+++ src/main/java/net/mavity/the_legend_of_wiiu/the_legend_of_wiiu.java
@@ -48,6 +48,7 @@ public class the_legend_of_wiiu
                 output.accept(ModItems.GOLD_RUPEE.get());
                 output.accept(ModItems.ROYAL_GUARDS_SWORD.get());
                 output.accept(ModBlocks.EXAMPLE_BLOCK.get());
+                output.accept(ModItems.THE_MASTER_SWORD.get());
             }).build());
 
     // Define mod id in a common place for everything to reference
