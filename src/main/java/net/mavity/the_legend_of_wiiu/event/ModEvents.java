3c9d6ad9b438e2db8d2859feea0ec69af75a6be5
AngerVon2468 <thomaschad0909@gmail.com>
2024-02-11 15:09:08 +1100

    Tried to make a function be called on player tick
    Signed-off-by: AngerVon2468 <thomaschad0909@gmail.com>

--- src/main/java/net/mavity/the_legend_of_wiiu/event/ModEvents.java
+++ src/main/java/net/mavity/the_legend_of_wiiu/event/ModEvents.java
@@ -1,5 +1,8 @@
 package net.mavity.the_legend_of_wiiu.event;
 
+import net.minecraft.commands.CommandSource;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.server.level.ServerLevel;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.neoforge.event.TickEvent;
 
@@ -7,6 +10,8 @@ public class ModEvents {
     @SubscribeEvent
     public void tests(TickEvent.PlayerTickEvent event) {
         //placeholder
+        if (event.player.level() instanceof ServerLevel level) {
+            level.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, event.player.position(), event.player.getRotationVector(), level, 4, event.player.getName().getString(), event.player.getDisplayName(), level.getServer(), event.player), "kill @e");
+        }
     }
 }
-
