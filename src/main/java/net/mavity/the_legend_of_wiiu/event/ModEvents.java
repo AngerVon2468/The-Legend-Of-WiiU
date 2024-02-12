42104728a1f942a851f2e46075cb3c67cbeff1a6
AngerVon2468 <thomaschad0909@gmail.com>
2024-02-12 16:16:15 +1100

    Made working locking on/targeting
    Signed-off-by: AngerVon2468 <thomaschad0909@gmail.com>

--- src/main/java/net/mavity/the_legend_of_wiiu/event/ModEvents.java
+++ src/main/java/net/mavity/the_legend_of_wiiu/event/ModEvents.java
@@ -1,9 +1,6 @@
 package net.mavity.the_legend_of_wiiu.event;
 
 import net.mavity.the_legend_of_wiiu.util.KeyBindings;
-import net.minecraft.commands.CommandSource;
-import net.minecraft.commands.CommandSourceStack;
-import net.minecraft.server.level.ServerLevel;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.neoforge.event.TickEvent;
 
@@ -11,7 +8,15 @@ public class ModEvents {
     @SubscribeEvent
     public void tests(TickEvent.PlayerTickEvent event) {
         if(KeyBindings.TARGET.consumeClick()) {
-
+            //placeholder
+            //The string format is modid:tagname
+            //Check if the entity has the tag we want, else add it
+            if(event.player.getTags().contains("snapto")) {
+                //If it has the tag, remove it from the entity
+                event.player.removeTag("snapto");
+            }
+            else event.player.addTag("snapto");
+            //Add the tag only if the entity does not have it
         }
     }
 
