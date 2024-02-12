abadd6dc92e212f19149ff7e7d485b65f3e178e8
AngerVon2468 <thomaschad0909@gmail.com>
2024-02-12 13:18:46 +1100

    Registered a new block
    Signed-off-by: AngerVon2468 <thomaschad0909@gmail.com>

--- src/main/java/net/mavity/the_legend_of_wiiu/block/ModBlocks.java
+++ src/main/java/net/mavity/the_legend_of_wiiu/block/ModBlocks.java
@@ -15,5 +15,8 @@ public class ModBlocks {
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(the_legend_of_wiiu.MODID);
     public static final DeferredBlock<Block> EXAMPLE_BLOCK = BLOCKS.registerSimpleBlock("example_block", BlockBehaviour.Properties.of().mapColor(MapColor.STONE));
     // Creates a new BlockItem with the id "examplemod:example_block", combining the namespace and path
+    public static final DeferredBlock<Block> PED_WITH_MS = BLOCKS.registerSimpleBlock("ped_with_ms", BlockBehaviour.Properties.of().noOcclusion().mapColor(MapColor.STONE));
     public static final DeferredItem<BlockItem> EXAMPLE_BLOCK_ITEM = ITEMS.registerSimpleBlockItem("example_block", EXAMPLE_BLOCK);
+    public static final DeferredItem<BlockItem> PED_WITH_MS_ITEM = ITEMS.registerSimpleBlockItem("ped_with_ms", PED_WITH_MS);
+    //Registers an item for the block so you dont crash when its in your inventory
 }
