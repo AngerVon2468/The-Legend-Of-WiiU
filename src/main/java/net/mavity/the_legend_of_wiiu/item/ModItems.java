c0920f7bfb91cef90bb877581b3e3107826c291c
AngerVon2468 <thomaschad0909@gmail.com>
2024-02-22 12:24:33 +1100

    fixed durability and itemstack size on some items
    Signed-off-by: AngerVon2468 <thomaschad0909@gmail.com>

--- src/main/java/net/mavity/the_legend_of_wiiu/item/ModItems.java
+++ src/main/java/net/mavity/the_legend_of_wiiu/item/ModItems.java
@@ -11,43 +11,43 @@ public class ModItems {
             DeferredRegister.createItems(the_legend_of_wiiu.MODID);
 
     public static final DeferredItem<Item> BLUE_RUPEE = ITEMS.register("blue_rupee",
-            () -> new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties().durability(0)));
 
     public static final DeferredItem<Item> GREEN_RUPEE = ITEMS.register("green_rupee",
-            () -> new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties().durability(0)));
 
     public static final DeferredItem<Item> YELLOW_RUPEE = ITEMS.register("yellow_rupee",
-            () -> new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties().durability(0)));
 
     public static final DeferredItem<Item> RED_RUPEE = ITEMS.register("red_rupee",
-            () -> new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties().durability(0)));
 
     public static final DeferredItem<Item> PURPLE_RUPEE = ITEMS.register("purple_rupee",
-            () -> new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties().durability(0)));
 
     public static final DeferredItem<Item> SILVER_RUPEE = ITEMS.register("silver_rupee",
-            () -> new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties().durability(0)));
 
     public static final DeferredItem<Item> GOLD_RUPEE = ITEMS.register("gold_rupee",
-            () -> new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties().durability(0)));
 
     public static final DeferredItem<Item> ROYAL_GUARDS_SWORD = ITEMS.register("royal_guards_sword",
             () -> new SwordItem(Tiers.WOOD, 7, -2.4f,
-                    new Item.Properties()));
+                    new Item.Properties().durability(64)));
 
     public static final DeferredItem<Item> THE_MASTER_SWORD = ITEMS.register("the_master_sword",
             () -> new SwordItem(Tiers.DIAMOND, 8, -2.4f,
-                    new Item.Properties()));
+                    new Item.Properties().durability(552)));
 
     public static final DeferredItem<Item> THE_AWAKENED_MASTER_SWORD = ITEMS.register("the_awakened_master_sword",
             () -> new SwordItem(Tiers.DIAMOND, 14, -2.4f,
-                    new Item.Properties()));
+                    new Item.Properties().durability(1024)));
 
     public static final DeferredItem<Item> MAJORAS_MASK = ITEMS.register("majoras_mask",
-            () -> new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties().stacksTo(1).durability(0)));
 
     public static final DeferredItem<Item> THE_WIIU = ITEMS.register("wiiu",
-            () -> new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties().stacksTo(1).durability(0)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
