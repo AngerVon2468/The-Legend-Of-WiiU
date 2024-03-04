5941bca7a344159e976e31b7baf488131b1533a6
AngerVon2468 <thomaschad0909@gmail.com>
2024-03-04 17:16:14 +1100

    Updated the rupee properties
    Signed-off-by: AngerVon2468 <thomaschad0909@gmail.com>

--- src/main/java/net/mavity/the_legend_of_wiiu/item/ModItems.java
+++ src/main/java/net/mavity/the_legend_of_wiiu/item/ModItems.java
@@ -6,30 +6,32 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
+import static net.mavity.the_legend_of_wiiu.propertymanager.PropertyInit.RUPEETYPE;
+
 public class ModItems {
     public static final DeferredRegister.Items ITEMS =
             DeferredRegister.createItems(the_legend_of_wiiu.MODID);
 
     public static final DeferredItem<Item> BLUE_RUPEE = ITEMS.register("blue_rupee",
-            () -> new Item(new Item.Properties().durability(0)));
+            () -> new Item(RUPEETYPE));
 
     public static final DeferredItem<Item> GREEN_RUPEE = ITEMS.register("green_rupee",
-            () -> new Item(new Item.Properties().durability(0)));
+            () -> new Item(RUPEETYPE));
 
     public static final DeferredItem<Item> YELLOW_RUPEE = ITEMS.register("yellow_rupee",
-            () -> new Item(new Item.Properties().durability(0)));
+            () -> new Item(RUPEETYPE));
 
     public static final DeferredItem<Item> RED_RUPEE = ITEMS.register("red_rupee",
-            () -> new Item(new Item.Properties().durability(0)));
+            () -> new Item(RUPEETYPE));
 
     public static final DeferredItem<Item> PURPLE_RUPEE = ITEMS.register("purple_rupee",
-            () -> new Item(new Item.Properties().durability(0)));
+            () -> new Item(RUPEETYPE));
 
     public static final DeferredItem<Item> SILVER_RUPEE = ITEMS.register("silver_rupee",
-            () -> new Item(new Item.Properties().durability(0)));
+            () -> new Item(RUPEETYPE));
 
     public static final DeferredItem<Item> GOLD_RUPEE = ITEMS.register("gold_rupee",
-            () -> new Item(new Item.Properties().durability(0)));
+            () -> new Item(RUPEETYPE));
 
     public static final DeferredItem<Item> ROYAL_GUARDS_SWORD = ITEMS.register("royal_guards_sword",
             () -> new SwordItem(Tiers.WOOD, 7, -2.4f,
